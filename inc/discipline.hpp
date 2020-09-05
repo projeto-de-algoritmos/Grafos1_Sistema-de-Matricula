895ca903aa42e817fc38717f721b9a7dfc956865
Lucas <lucasfcm9@gmail.com>
2020-09-04 22:12:10 -0300

    Change discipline.cpp, discipline.hpp and update csv file

--- inc/discipline.hpp
+++ inc/discipline.hpp
@@ -5,9 +5,14 @@ class Discipline {
  public:
   string name;
   int code;
+  string prerequisite1;
+  int code1;
+  string prerequisite2;
+  int code2;
 
   Discipline() {}
-  Discipline(string _name, int _code) : name(_name), code(_code) {}
+  Discipline(string _name, int _code, string _prerequisite1, int _code1, string _prerequisite2, int _code2) : name(_name),
+             code(_code), prerequisite1(_prerequisite1), code1(_code1), prerequisite2(_prerequisite2), code2(_code2) {}
   ~Discipline() {}
   vector<Discipline> read_file(const string& path);
 };
--- src/discipline.cpp
+++ src/discipline.cpp
@@ -8,27 +8,56 @@ vector<Discipline> Discipline::read_file(const string& path) {
   if (!fp) {
     cout << "unable to open file!" << '\n';
   }
-  int buffer = 40, pos = 0;
+  int buffer = 100, pos = 0;
   fp.ignore(buffer, '\n');
   string line;
   vector<Discipline> dis;
-  string _s, _c;
+  string d, c, pr1, c1, pr2, c2;
+  int j = 0, k = 0, l = 0, p = 0, x = 0;
   while (getline(fp, line, '\n')) {
     for (int i = 0; i < (int) line.size(); i++) {
       if (line[i] == ',') {
         pos = i;
         break;
       }
-      _s += line[i];
+      d += line[i];
     }
-    for (int j = pos + 1; j < (int) line.size(); j++) {
-      if (line[j] == '\0') {
+    for (j = pos + 1; j < (int) line.size(); j++) {
+      if (line[j] == ',') {
+        pos = j;
         break;
       }
-      _c += line[j];
+      c += line[j];
     }
-    dis.push_back({_s, stoi(_c)});
-    _s = _c = "";
+    for (k = pos + 1; k < (int) line.size(); k++) {
+      if (line[k] == ',') {
+        pos = k;
+        break;
+      }
+      pr1 += line[k];
+    }
+    for (l = pos + 1; l < (int) line.size(); l++) {
+      if (line[l] == ',') {
+        pos = l;
+        break;
+      }
+      c1 += line[l];
+    }
+    for (p = pos + 1; p < (int) line.size(); p++) {
+      if (line[p] == ',') {
+        pos = p;
+        break;
+      }
+      pr2 += line[p];  
+    }
+    for (x = pos + 1; x < (int) line.size(); x++) {
+      if (line[x] == '\0') {
+        break;
+      }
+      c2 += line[x];  
+    }
+    dis.push_back({d, stoi(c), pr1, stoi(c1), pr2, stoi(c2)});
+    d = c = pr1 = c1 = pr2 = c2 = "";
   }
   return dis;
 }
